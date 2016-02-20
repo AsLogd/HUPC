32b8fa4f0498dd8a26f472287ae4bb6d855123b9
Eduardo Ramírez Farias <eedmolteen@gmail.com>
2016-02-20 12:34:08 +0100

    tryin

--- index.js
+++ index.js
@@ -26,6 +26,25 @@ io.on('connection', function(socket){
     socket.broadcast.emit("rotation", o);
   });
 
+  socket.on('move', function(o){
+    socket.broadcast.emit("move",o);
+  });
+  /*
+  socket.on('moving_up', function(o){
+    socket.broadcast.emit("moving_up");
+  });
+  socket.on('moving_left', function(o){
+    socket.broadcast.emit("moving_left");
+  });
+  socket.on('moving_right', function(o){
+    socket.broadcast.emit("moving_right");
+  });
+  socket.on('moving_down', function(o){
+    socket.broadcast.emit("moving_down");
+  });
+  socket.on('stop_moving', function(o){
+    socket.broadcast.emit("stop_moving");
+  });*/
 });
 
 http.listen(3000, function(){
